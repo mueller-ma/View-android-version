e64c6841f930c0305cc5aa3d1035fc828fe6120b
mueller-ma <mueller-ma@users.noreply.github.com>
2021-05-14 15:40:26 +0200

    Code style

--- app/src/main/java/com/github/mueller_ma/viewandroidversion/MainActivity.java
+++ app/src/main/java/com/github/mueller_ma/viewandroidversion/MainActivity.java
@@ -1,11 +1,12 @@
 package com.github.mueller_ma.viewandroidversion;
 
 import android.os.Build;
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Html;
 import android.text.method.LinkMovementMethod;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.Locale;
 
@@ -20,45 +21,56 @@ public class MainActivity extends AppCompatActivity {
         String versionName;
         switch (sdkVersion) {
             case 14:
-            case 15: versionName = "Ice Cream Sandwich";
-            break;
+            case 15:
+                versionName = "Ice Cream Sandwich";
+                break;
             case 16:
             case 17:
-            case 18: versionName = "Jelly Bean";
-            break;
+            case 18:
+                versionName = "Jelly Bean";
+                break;
             case 19:
-            case 20: versionName = "Kitkat";
-            break;
+            case 20:
+                versionName = "Kitkat";
+                break;
             case 21:
-            case 22: versionName = "Lollipop";
-            break;
-            case 23: versionName = "Marshmallow";
-            break;
+            case 22:
+                versionName = "Lollipop";
+                break;
+            case 23:
+                versionName = "Marshmallow";
+                break;
             case 24:
-            case 25: versionName = "Nougat";
-            break;
+            case 25:
+                versionName = "Nougat";
+                break;
             case 26:
-            case 27: versionName = "Oreo";
-            break;
-            case 28: versionName = "Pie";
-            break;
-            case 29: versionName = "Q";
-            break;
-            case 30: versionName = "R";
-            break;
-            default: versionName = getString(R.string.unknown);
+            case 27:
+                versionName = "Oreo";
+                break;
+            case 28:
+                versionName = "Pie";
+                break;
+            case 29:
+                versionName = "Q";
+                break;
+            case 30:
+                versionName = "R";
+                break;
+            default:
+                versionName = getString(R.string.unknown);
         }
 
         TextView versionTextView = findViewById(R.id.version);
         versionTextView.setText(String.format(getString(R.string.codename), versionName));
-        versionTextView.append(String.format(Locale.US,getString(R.string.version), Build.VERSION.RELEASE, sdkVersion));
+        versionTextView.append(String.format(Locale.US, getString(R.string.version), Build.VERSION.RELEASE, sdkVersion));
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             String baseOs = Build.VERSION.BASE_OS;
             if (baseOs.length() > 0) {
                 versionTextView.append(String.format(getString(R.string.based_on), baseOs));
             }
             int prevSDK = Build.VERSION.PREVIEW_SDK_INT;
-            if(prevSDK != 0) {
+            if (prevSDK != 0) {
                 versionTextView.append(String.format(Locale.US, getString(R.string.preview), Build.VERSION.CODENAME, prevSDK));
             }
             versionTextView.append(String.format(getString(R.string.patch), Build.VERSION.SECURITY_PATCH));
